bd4a2d1a25f4aeb7c2709c6fa7c52b68ac6065db
samvrny <samvrny@gmail.com>
2022-09-20 15:13:44 -0500

    added reply to comment functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -37,7 +37,7 @@ const PizzaSchema = new Schema(
 
 //get total counts of comments and replies on retrievals
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 })
 
 //create the Pizza model using the PizzaSchema
